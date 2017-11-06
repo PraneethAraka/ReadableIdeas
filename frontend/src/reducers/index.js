b79e0d718bd68597eda7347c4ff06e6185deed91
Praneeth Reddy Araka <praraka@mix.wvu.edu>
2017-11-06 15:04:48 -0500

    Create index.js

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux';
+
+import {
+  ADD_POST,
+  ADD_COMMENTS
+} from '../actions'
+
+function post (state = {}, action) {
+  if (action.type === "ADD_POST")
+      {
+      return action.state+action.voteScore;
+      }
+      return state;
+}
+
+function comment (state = {}, action) {
+  if (action.type === "ADD_POST")
+      {
+      return action.state+action.voteScore;
+      }
+      return state;
+}
+
+export default combineReducers({
+  post,
+  comment
+})
